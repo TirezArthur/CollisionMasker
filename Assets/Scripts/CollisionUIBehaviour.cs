5d3c73eeca7db64e97fa0035b1286acc8dbd9093
MatthiasOoms <matthias.ooms1@gmail.com>
2026-01-31 13:06:10 +0100

    Added locking layer controls to Collision UI Grid

--- Assets/Scripts/CollisionUIBehaviour.cs
+++ Assets/Scripts/CollisionUIBehaviour.cs
@@ -15,6 +15,8 @@ public class CollisionUIBehaviour : MonoBehaviour
     [SerializeField] private GameObject m_TogglePrefab;
     [SerializeField] private GameObject m_LabelPrefab;
 
+    [SerializeField] private LayerMask m_LockedLayers;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -112,8 +114,15 @@ public class CollisionUIBehaviour : MonoBehaviour
                     // TODO: Implement initial grid state if needed
 
                     // Set layers
-                    toggleButton.GetComponent<CollisionToggleButton>().m_IgnoreLayer1 = LayerMask.GetMask(layerName);
-                    toggleButton.GetComponent<CollisionToggleButton>().m_IgnoreLayer2 = LayerMask.GetMask(defaultLayerName);
+                    CollisionToggleButton collisionToggle = toggleButton.GetComponent<CollisionToggleButton>();
+                    collisionToggle.m_IgnoreLayer1 = LayerMask.GetMask(layerName);
+                    collisionToggle.m_IgnoreLayer2 = LayerMask.GetMask(defaultLayerName);
+
+                    if ((m_LockedLayers.value & (1 << leftIdx)) != 0 ||
+                        (m_LockedLayers.value & (1 << topIdx)) != 0)
+                    {
+                        collisionToggle.SetToggleUnlocked(false);
+                    }
 
                     // Adjust grid size to fit new button
                     if (layerCount < 1)
