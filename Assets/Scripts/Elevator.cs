2a6906e02692667301227679dd2cf9e783d945a7
MatthiasOoms <matthias.ooms1@gmail.com>
2026-02-01 02:24:14 +0100

    Elevator wait time implemented

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,7 +4,10 @@ public class Elevator : MonoBehaviour
 {
     Vector3 _origPosition;
     private float _startTime;
-    
+
+    private float _timer = 0f;
+    private int _timerMax = 1;
+
     void Start()
     {
         _origPosition = transform.localPosition;
@@ -13,6 +16,22 @@ public class Elevator : MonoBehaviour
 
     void FixedUpdate()
     {
+        // If a full sine wave has passed
+        if ((Time.time - _startTime) >= 8)
+        {
+            _timer += Time.fixedDeltaTime;
+
+            if (_timer >= _timerMax)
+            {
+                _startTime = Time.time;
+                _timer = 0f;
+            }
+            else
+            {
+                return;
+            }
+        }
+
         Vector3 position = _origPosition;
         position.y += 2 * Mathf.Abs(Mathf.Sin(Mathf.Deg2Rad * 90 * (Time.time-_startTime) / 4));
         transform.localPosition = position;
